6668c3f1d6348f6ba2cc90bdacf11f8de64fa687
kacper <kacperadach@gmail.com>
2017-01-22 17:14:08 -0500

    falcons are getting piped

--- game_logs.py
+++ game_logs.py
@@ -12,6 +12,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
+from options import TEAMS, SEASON_TYPES
+
 CHROMEDRIVER = '/home/kacper/apps/chromedriver/chromedriver'
 
 BASE_URL = 'http://stats.nba.com/search/player-game'
@@ -21,14 +23,14 @@ run_button_label = '.run-it'
 close_stat_filter_label = '.close'
 stat_filter_label = '.inner'
 season_button_label = '.multiselect-info'
-season_options_label = '.multiselect-box__option'
+options_label = '.multiselect-box__option'
 pagination_button_label = '.querytool-pagination__more'
 
-def read_game_logs(season):
+def read_game_logs(season, season_type, team):
     driver = get_page_driver(BASE_URL)
     #soup = BS(driver.page_source, 'html.parser')
     close_stat_filter(driver)
-    select_season(driver, season)
+    select_options(driver, season, season_type, team)
     press_run_button(driver)
     open_all_logs(driver)
     rows = get_table_rows(driver)
@@ -37,8 +39,7 @@ def read_game_logs(season):
 
 def get_table_rows(driver):
 	table = driver.find_element_by_css_selector(stat_table_label)
-	rows = table.find_elements_by_tag_name('tr')
-	rows = filter(lambda x: x.text != '', rows)
+	rows = filter(lambda x: x.text != '', table.find_elements_by_tag_name('tr'))
 	Tracer()()
 	return rows
 	
@@ -58,18 +59,28 @@ def close_stat_filter(driver):
 	close_button = driver.find_element_by_css_selector(close_stat_filter_label).find_elements_by_css_selector("*")[0]
 	close_button.click()
 
-def select_season(driver, season):
+def select_options(driver, season, season_type, team):
 	# season should be a string in the format "2016-17"
-	season_button = driver.find_element_by_css_selector(season_button_label)
-	season_button.click()
-	buttons = driver.find_elements_by_css_selector(season_options_label)
-	option = None
-	for b in buttons:
-		if b.text == season:
-			option = b
-			break
-	if option:
-		option.click()
+	# season_type should be: "Preseason", "Regular Season", "Playoffs", "All Star"
+	buttons = driver.find_elements_by_css_selector(season_button_label)
+	season_button = buttons[0]
+	season_type_button = buttons[1]
+	team_button = buttons[2]
+
+	def select(button, val):
+		button.click()
+		buttons = driver.find_elements_by_css_selector(options_label)
+		for b in buttons:
+			if b.text == val:
+				option = b
+				break
+		if option:
+			option.click()
+		button.click()
+
+	select(season_button, season)
+	select(season_type_button, season_type)
+	select(team_button, team)
 
 def wait_for_load(driver, delay=15):
 	try:
@@ -89,7 +100,6 @@ def take_screenshot(driver):
 
 def get_page_driver(url):
 	driver = webdriver.Chrome()
-	#driver = webdriver.Chrome()
 	driver.get(url)
 	return driver
 
@@ -104,11 +114,13 @@ if __name__ == "__main__":
 	second = date(year=1986, month=1, day=1)
 	while first.year < 2017:
 		season = create_year_str(first, second)
-		print 'Season {}'.format(season)
-		#read_game_logs('2016-17')
-		try:
-			read_game_logs(season)
-		except TimeoutException:
-			pass
+		for season_type in SEASON_TYPES:
+			for team in TEAMS:
+				#read_game_logs('2016-17')
+				print "{} - {} - {}".format(season, season_type, team)
+				try:
+					read_game_logs(season, season_type, team)
+				except TimeoutException:
+					pass
 		first = first + timedelta(366)
 		second = second + timedelta(366)
